d05b1626dd7532f54fc6371785232d718dfa211c
bukvic6 <bukvic6@gmail.com>
2023-06-01 20:47:58 +0200

    validations

--- src/components/CreateProcPlan.jsx
+++ src/components/CreateProcPlan.jsx
@@ -2,45 +2,78 @@ import { useState } from 'react'
 import ProcurementService from '../services/ProcurementService';
 import './CreateProcPlan.css'
 import { useNavigate } from 'react-router-dom';
-export default function CreateProcurementPlan(){
-    const[productType, setProductType] = useState("");
-    const[estimatedValue, setEstimatedValue] = useState("");
-    const[quantity, setQuantity] = useState("");
-    
-const navigate = useNavigate();  
+export default function CreateProcurementPlan() {
+  const [productType, setProductType] = useState("");
+  const [estimatedValue, setEstimatedValue] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [formErrors, setFormErrors] =useState({})
 
-    const handleSubmit = async (e) =>{
-        e.preventDefault();    
-        const procurementPlan = { product_type:productType, estimated_value:estimatedValue,quantity:parseInt(quantity)}
-        try{
-            const resp = await ProcurementService.createProcurementPlan(procurementPlan);
-            navigate("/");
 
-        } catch(error){
-            console.log(error)
-        }
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if(!validateForm()){
+      return;
     }
-    return(
-        <div className="createProcurementplan">
-        <form className="form-style-9" onSubmit={handleSubmit}>
-            <div class="cardHeader">
-                    <h2>Kreiranje Plana Javne Nabavke</h2>
-            </div>
+    const procurementPlan = { product_type: productType, estimated_value: estimatedValue, quantity: parseInt(quantity) }
+    try {
+      const resp = await ProcurementService.createProcurementPlan(procurementPlan);
+      navigate("/");
 
-          <ul>
-            <li>
-              <input type="text" name="productType" value={productType} onChange={(e) => setProductType(e.target.value)} className="field-style field-split align-left" placeholder="Tip Proizvoda" />
-              <input type="number" name="quantity" value={quantity} onChange={(e) => setQuantity(e.target.value)} className="field-style field-split align-right" placeholder="Kolicina" />            </li>
-            <li>
-              <input type="text" name="estimatedValue" value={estimatedValue}  onChange={(e) => setEstimatedValue(e.target.value)} className="field-style field-full align-none" placeholder="Procenjena Vrednost" />
-            </li>
-
-            <li>
-              <input type="submit" value="Zavrsi" />
-            </li>
-          </ul>
-        </form>
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  const validateForm = () => {
+    const errors = {}
+    let isValid = true;
+    if(!productType.trim()){
+      errors.productType = "Tip proizvoda je obavezan";
+      isValid = false;
+    }
+    if(!estimatedValue.trim()){
+      errors.estimatedValue = "Procenjena vrednost je obavezna";
+      isValid = false;
+    }
+    if(!quantity.trim()){
+      errors.quantity = "Količina je obavezna";
+      isValid = false;
+    }
+    setFormErrors(errors);
+    return isValid;
+  }
+  return (
+    <div className="createProcurementplan">
+      <form className="form-style-9" onSubmit={handleSubmit}>
+        <div class="cardHeader">
+          <h2>Kreiranje Plana Javne Nabavke</h2>
         </div>
 
-    )
+        <ul>
+          <li>
+            <input type="text" name="productType" value={productType} onChange={(e) => setProductType(e.target.value)} className="field-style field-split align-left" placeholder="Tip Proizvoda" />
+            <input type="number" name="quantity" value={quantity} onChange={(e) => setQuantity(e.target.value)} className="field-style field-split align-right" placeholder="Količina" />  
+            {formErrors.productType && (
+              <p className="error red-text">{formErrors.productType}</p>
+            )}
+            {formErrors.quantity && (
+              <p className="error red-text">{formErrors.quantity}</p>
+            )}        
+            </li>
+          <li>
+            <input type="text" name="estimatedValue" value={estimatedValue} onChange={(e) => setEstimatedValue(e.target.value)} className="field-style field-full align-none" placeholder="Procenjena Vrednost" />
+            {formErrors.estimatedValue && (
+              <p className="error red-text">{formErrors.estimatedValue}</p>
+            )}
+          </li>
+
+          <li>
+            <input type="submit" value="Zavrsi" />
+          </li>
+        </ul>
+      </form>
+    </div>
+
+  )
 }
--- src/components/CreateProcurement.jsx
+++ src/components/CreateProcurement.jsx
@@ -1,16 +1,17 @@
 import './CreateProcurement.css'
 import ProcurementService from '../services/ProcurementService';
-import { useState , useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import DatePicker from "react-datepicker"
 import "react-datepicker/dist/react-datepicker.css";
 import { useNavigate } from 'react-router-dom';
 export default function CreateProcurement() {
-  const[procurementName, setProcurementName] = useState("");
-  const[description, setDescription] = useState("");
-  const[procurementPlanId, setProcurementPlanId] = useState("");
+  const [procurementName, setProcurementName] = useState("");
+  const [description, setDescription] = useState("");
+  const [procurementPlanId, setProcurementPlanId] = useState("");
   const [plans, setPlans] = useState([]);
-  const[endDate, setEndDate] = useState(null)
-  
+  const [endDate, setEndDate] = useState(null)
+  const [formErrors, setFormErrors] = useState({});
+
   useEffect(() => {
     fetchPlans();
   }, []);
@@ -24,34 +25,67 @@ export default function CreateProcurement() {
     }
   };
 
-  const navigate = useNavigate();  
-  const handleSubmit = async (e) =>{
-      e.preventDefault();    
-      const procurement = { procurement_name:procurementName, procurement_plan_id:procurementPlanId, end_date:endDate, description:description}
-      try{
-          const resp = await ProcurementService.createProcurement(procurement);
-          navigate("/");
-          
-      } catch(error){
-          console.log(error.response)
-      }
-  }
-    return (
-      <div className="createProcurement">
-        <form className="form-style-9" onSubmit={handleSubmit}>
-            <div class="cardHeader">
-                    <h2>Kreiranje Javne Nabavke</h2>
-            </div>
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!validateForm()) {
+      return;
+    }
+    const procurement = {
+      procurement_name: procurementName,
+      procurement_plan_id: procurementPlanId,
+      end_date: endDate,
+      description: description,
+    };
 
-          <ul>
-            <li>
+    try {
+      const resp = await ProcurementService.createProcurement(procurement);
+      navigate("/");
+
+    } catch (error) {
+      console.log(error.response)
+    }
+  }
+  const validateForm = () => {
+    const errors = {};
+    let isValid = true;
+
+    if (!procurementName.trim()) {
+      errors.procurementName = "Naziv Javne Nabavke je obavezan.";
+      isValid = false;
+    }
+
+    if (!description.trim()) {
+      errors.description = "Opis Javne Nabavke je obavezan.";
+      isValid = false;
+    }
+
+    if (!procurementPlanId) {
+      errors.procurementPlanId = "Morate odabrati Plan Javne Nabavke.";
+      isValid = false;
+    }
+
+    setFormErrors(errors);
+    return isValid;
+  };
+  return (
+    <div className="createProcurement">
+      <form className="form-style-9" onSubmit={handleSubmit}>
+        <div class="cardHeader">
+          <h2>Kreiranje Javne Nabavke</h2>
+        </div>
+        <ul>
+          <li>
             <input type="text" name="procurementName" value={procurementName} onChange={(e) => setProcurementName(e.target.value)} className="field-style field-full align-none" placeholder="Naziv Javne Nabavke" />
-            </li>
-            <li>
-              <DatePicker selected={endDate} isClearable minDate={new Date()} onChange={date => setEndDate(date)}
-              ></DatePicker>
-            </li>
-            <li className="dropdown">
+            {formErrors.procurementName && (
+              <p className="error red-text">{formErrors.procurementName}</p>
+            )}
+          </li>
+          <li>
+            <DatePicker selected={endDate} isClearable minDate={new Date()} onChange={date => setEndDate(date)}
+            ></DatePicker>
+          </li>
+          <li className="dropdown">
             <label htmlFor="plan" className="plan">
               Odaberite Plan Javne Nabavke:
             </label>
@@ -59,25 +93,27 @@ export default function CreateProcurement() {
               name="plan"
               id="language"
               value={procurementPlanId}
-              onChange={(e) => setProcurementPlanId(e.target.value)}
-            >
+              onChange={(e) => setProcurementPlanId(e.target.value)}>
               {plans.map((plan) => (
                 <option key={plan.procurement_plan_id} value={plan.procurement_plan_id}>
-                  {plan.product_type + " " +plan.estimated_value + " " + plan.quantity} {}
+                  {plan.product_type + " cena: " + plan.estimated_value + " količina: " + plan.quantity} { }
                 </option>
               ))}
             </select>
+            {formErrors.procurementPlanId && (
+              <p className="error red-text">{formErrors.procurementPlanId}</p>
+            )}
           </li>
-
-
-            <li>
-              <textarea name="description"  value={description} onChange={(e) => setDescription(e.target.value)} className="field-style" placeholder="Opis Javne Nabavke"></textarea>
-            </li>
-            <li>
-              <input type="submit" value="Zavrsi" />
-            </li>
-          </ul>
-        </form>
-      </div>
-    );
-  }
+          <li>
+            <textarea name="description" value={description} onChange={(e) => setDescription(e.target.value)} className="field-style" placeholder="Opis Javne Nabavke"></textarea>
+            {formErrors.description && (
+              <p className="error red-text">{formErrors.description}</p>)}
+          </li>
+          <li>
+            <input type="submit" value="Zavrsi" />
+          </li>
+        </ul>
+      </form>
+    </div>
+  );
+}
